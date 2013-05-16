8666b3a5bcf6a2b97221798c230fcc19e560b933
Gabriella Alves <gabi.mayara@dce.ufpb.br>
2013-05-16 02:38:00 -0300

    Alo Mundo!

---
+++ src/AloMundo.java
@@ -0,0 +1,12 @@
+
+public class AloMundo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Alo Mundo!");
+	}
+
+}
