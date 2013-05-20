365c8cf7e81f3c3b213b90fd1981224576dc793a
Marcelo Nobrega e Silva <marcelo.nobrega@dce.ufpb.br>
2013-05-20 16:11:30 -0300

    testando o git

--- src/AloMundo.java
+++ src/AloMundo.java
@@ -7,6 +7,7 @@ public class AloMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Alo Mundo!");
+		System.out.println("testando o git Marcelo Nóbrega");
 	}
 
 }
