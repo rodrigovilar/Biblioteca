bb6caaab754fdd04cf658a67ac4d242743ba22a2
Gabriella Alves <gabi.mayara@dce.ufpb.br>
2013-06-09 02:40:02 -0300

    Alterações

---
+++ src/br/com/aps/controle/GerenteCurso.java
@@ -0,0 +1,50 @@
+package br.com.aps.controle;
+import java.util.ArrayList;
+import java.util.List;
+import br.com.aps.entidade.Curso;
+import br.com.aps.excecao.Excecao;
+
+public class GerenteCurso {
+
+	List<Curso> listaCursos= new ArrayList<Curso> ();
+
+	public void addCursos(Curso curso) {
+
+		if (curso.getNome() == null) {
+			throw new RuntimeException("o nome do curso é obrigatorio");
+		}
+		if (curso.getCodigo() == null) {
+			throw new RuntimeException("O codigo do curos é obrigatorio");
+		}
+		isExisteCurso(curso.getCodigo());
+		listaCursos.add(curso);
+	}
+
+	public void deletarCurso(Curso curso) {
+		retornarCurso(curso.getCodigo());
+		listaCursos.remove(curso);
+	}
+
+	public List<Curso> getListCurso() {
+		return listaCursos;
+	}
+
+	public Curso retornarCurso(String cod) {
+		for (Curso curso : listaCursos) {
+			if (curso.getCodigo() == cod)
+				return curso;
+		}
+		throw new Excecao("Nao existe Curso cadastrado com este Codigo");
+	}
+	
+	private void isExisteCurso(String codigoCurso){
+		for(Curso curso: listaCursos){
+			if(curso.getCodigo().equals(codigoCurso))
+				throw new Excecao("Curso já existente"); 
+		}
+	}
+	
+	
+	
+
+}
---
+++ src/br/com/aps/entidade/TipoPessoa.java
@@ -0,0 +1,6 @@
+package br.com.aps.entidade;
+
+public enum TipoPessoa {
+	
+	ALUNO, PROFESSOR, FUNCIONARIO
+}
